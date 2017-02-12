3b3e8afe53d85fbe1b4c7b226e04a631409a0dc6
Cameron Tullos <ctullos@resourceammirati.com>
2017-02-12 13:59:36 -0500

    RC: 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # handlebars-cond
 Handlebars helper that allows you to use comparison conditions like a typical IF statement.
+---
+##### Usage
+---
+```javascript
+{{#cond 0 '<' 300}}
+correct!
+{{/cond}}
+```
+
+```javascript
+{{#cond 0 '>' 300}}
+Are you sure?
+{{else}}
+I don't think so
+{{/cond}}
+```
+
+---
+##### Parameters
+---
+| Argument | Description  |
+|:----------|:------------|
+| lvalue    | The first value to compare.|
+| operator  | The comparison operator. Values: == === != !== < > <= >= typeof. |
+| rvalue    | The second value to compare.
---
+++ index.js
@@ -0,0 +1,54 @@
+/**
+ * Handlebars cond helper
+ * @param param1
+ * @param conditon {String} == === != !== < > <= >= typeof
+ * @param inc {Number} How much to increment per iteration.
+ */
+var test = function(lvalue, operator, rvalue) {
+    var operators, result;
+
+	if (arguments.length < 3) {
+		throw new Error("Handlerbars Helper 'cond' needs 2 parameters");
+	}
+
+	operators = {
+		'=='		: function(l, r) { return l == r; },
+		'==='		: function(l, r) { return l === r; },
+		'!='		: function(l, r) { return l != r; },
+		'!=='		: function(l, r) { return l !== r; },
+		'<'			: function(l, r) { return l < r; },
+		'>' 		: function(l, r) { return l > r; },
+		'<=' 		: function(l, r) { return l <= r; },
+		'>=' 		: function(l, r) { return l >= r; },
+		'typeof'	: function(l, r) { return typeof l == r; }
+	};
+
+	if (!operators[operator]) {
+		throw new Error("Handlerbars Helper 'cond' doesn't recognize the operator " + operator);
+	}
+
+	return operators[operator](lvalue, rvalue);
+};
+
+var cond = function(lvalue, operator, rvalue, options) {
+    var operators, result;
+
+	if (options === undefined) {
+		options 	= rvalue;
+		rvalue 		= operator;
+		operator 	= "===";
+	}
+
+	result = test(lvalue, operator, rvalue);
+
+	if (result) {
+		return options.fn(this);
+	} else {
+		return options.inverse(this);
+	}
+};
+
+module.exports = {
+	cond: cond,
+	test: test
+}
